e530d2ee8dec6136a7a96003c77370d95fcfbc34
杨昌国 <ycg-893@163.com>
2016-10-30 04:00:17 +0800

    初始化项目

---
+++ Surpass.ORM.Test/Generate/SqlBuildTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surpass.ORM.Test.Generate
+{
+    /// <summary>
+    /// Sql 生成器
+    /// </summary>
+    [TestFixture]
+    public class SqlBuildTest
+    {
+
+        //[Test]
+        //public void TestMethod1()
+        //{
+        //}
+    }
+}
---
+++ Surpass.ORM/Generate/SqlBuild.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surpass.ORM.Generate
+{
+    /// <summary>
+    /// Sql生成器
+    /// </summary>
+    public class SqlBuild
+    {
+
+    }
+}
